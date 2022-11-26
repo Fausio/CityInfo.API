0bf54268995f61cb3d38a51dc72997e0ce4b4fc1
fausio luis <fausioluis@live.com>
2022-11-26 13:13:02 +0200

    ICityRepository

---
+++ CityInfo.SERVICE/Repository/Interfaces/ICityRepository.cs
@@ -0,0 +1,15 @@
+﻿using CityInfo.DOMAIN.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityInfo.SERVICE.Repository.Interfaces
+{
+    public interface ICityRepository
+    {
+        // IQueryable: you can order by, make where, etc etc
+       Task<IEnumerable<City>> GetCities();
+    }
+}
